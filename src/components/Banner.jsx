650e27b9dfd84f20b7a4c9e1d2f568ecddb094de
imteajalammonna <imteajalammonna@gmail.com>
2024-01-19 23:14:51 +0600

    somthing edited

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -7,7 +7,7 @@ const Banner = ({ autoPlayInterval }) => {
     const slides = [
         { imgSrc: "https://i.ibb.co/5T5766N/phono-slider-1.webp", text: "EXCLUSIVE STEEL FRAME", textPosition: "left-[75%] ", color: 'text-white', button: 'white-btn' },
         { imgSrc: "https://i.ibb.co/ZRsm2c9/phono-slider-2.webp", text: "Discover Exciting Offers", textPosition: "right-[10%] left-[38%]", color: 'text-black', button: 'black-btn' },
-        { imgSrc: "https://i.ibb.co/55Tq4DY/phono-slider-3.webp", text: "Join Us Today!", textPosition: "left-[75%]", color: 'text-white', button: 'white-btn' },
+        { imgSrc: "https://i.ibb.co/55Tq4DY/phono-slider-3.webp", text: "Join Us Today!", textPosition: "left-[78%]", color: 'text-white', button: 'white-btn' },
     ]
     const [currentSlide, setCurrentSlide] = useState(0);
 
@@ -32,10 +32,10 @@ const Banner = ({ autoPlayInterval }) => {
             <div className="flex transition-transform duration-500 ease-in-out" style={{ transform: `translateX(-${currentSlide * 100}%)` }}>
                 {slides.map((slide, index) => (
                     <div key={index} className="w-full flex-shrink-0 relative">
-                        <img src={slide.imgSrc} alt={`Slide ${index + 1}`} className="w-full h-auto" />
+                        <img src={slide.imgSrc} alt={`Slide ${index + 1}`} className="w-full h-full " />
                         <div className={`absolute ${slide.textPosition}`} style={{ top: '50%', transform: 'translate(-50%, -50%)' }}>
                             <div className={`text-xl md:text-8xl mb-4 md:mb-10 font-bold ${slide.color} `} >{slide.text}</div>
-                            <button className={`p-2 md:p-3 px-2 md:px-8 ${slide.button}`}>Buy Now</button>
+                            <button className={`p-2 py-1 md:p-3 text-[12px] md:text-lg md:px-10 ${slide.button}`}>Buy Now</button>
                         </div>
                     </div>
                 ))}
